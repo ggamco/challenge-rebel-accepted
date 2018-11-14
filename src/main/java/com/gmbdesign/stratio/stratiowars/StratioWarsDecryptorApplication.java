118c2b12066e91969bb421778e0c8c128c721688
ggamboa <ggamco@gmail.com>
2018-11-14 23:58:51 +0100

    Initial commit of Decryptor Rebel Tool

---
+++ src/main/java/com/gmbdesign/stratio/stratiowars/StratioWarsDecryptorApplication.java
@@ -0,0 +1,12 @@
+package com.gmbdesign.stratio.stratiowars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StratioWarsDecryptorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StratioWarsDecryptorApplication.class, args);
+	}
+}
